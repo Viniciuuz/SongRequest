626a2fa916de4cfd985e315e824673520949be31
Vinicius Matos <matosvinicius96@gmail.com>
2022-02-15 18:17:34 -0300

    noSongPlaylist is now actually a playlist Pog

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    while (currentIndex != 0) {
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
 const client = new tmi.Client({
     channels: [ urlParams.get('streamer') ],
     identity: {
