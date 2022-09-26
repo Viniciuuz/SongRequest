9cbc31ad2a20896380a70b8764ff00b6157583e2
Vinicius Matos <matosvinicius96@gmail.com>
2022-09-26 18:34:43 -0300

    noSongplaylist fixed

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ function shuffle(array) {
 const client = new tmi.Client({
     channels: [ urlParams.get('streamer') ],
     identity: {
-        username: 'song_request_bot',
-        password: 'oauth:4ne23tobrx889qe9l6xmaafqb6j8fb'
+      username: 'song_request_bot',
+      password: 'oauth:4ne23tobrx889qe9l6xmaafqb6j8fb'
     }
 });
 
